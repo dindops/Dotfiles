e8c900a68ea1a8f75398bdb162b929c726f0a99f
Piotr Dindorf <piotr.dindorf@mailo.com>
2023-01-29 23:31:14 +0100

    Fixes cpu_per.sh logic for my locale

--- scripts/.bin/cpu_per.sh
+++ scripts/.bin/cpu_per.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/sh
 
-top -bn1 | grep "Cpu(s)" | \
-               sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | \
-                          awk '{print 100 - $1"%"}'
+# top -bn1 - id gives % of idle CPU hence we subtract it from the 100
+top -bn1 | grep "Cpu(s)" | awk '{print 100 - $8"%"}'
