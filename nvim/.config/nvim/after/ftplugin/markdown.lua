2cb4870986fbbb4cd2edc95bd1f7e377b5b0a01b
Piotr Dindorf <piotr.dindorf@mailo.com>
2023-03-20 21:22:34 +0100

    Adds ftplugin dir to handle differences between filetypes

---
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -0,0 +1,3 @@
+vim.keymap.set("n", "<leader>b", "i**<ESC>Ea**<ESC>")
+vim.keymap.set("n", "<leader>i", "i*<ESC>Ea*<ESC>")
+vim.keymap.set("n", "<leader>c", "I```<ESC>o```<ESC>ka<Space>")
---
+++ nvim/.config/nvim/after/ftplugin/vimwiki.lua
@@ -0,0 +1,2 @@
+vim.keymap.set("n", "<Space><Space>", "<cmd>VimwikiToggleListItem<CR>")
+vim.keymap.set("n", "<Space>r", "<cmd>VimwikiToggleRejectedListItem<CR>")
