50ce0a3f2c5caf6ea41c4e8e800ecc405167d88e
Piotr Dindorf <piotr.dindorf@mailo.com>
2023-06-07 22:23:03 +0200

    Slight changes in required treesitter configuration

--- nvim/.config/nvim/lua/pietad/packer.lua
+++ nvim/.config/nvim/lua/pietad/packer.lua
@@ -15,7 +15,12 @@ return require('packer').startup(function(use)
 	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use {
+		'nvim-treesitter/nvim-treesitter',
+		run = function()
+			local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+			ts_update()
+		end,}
   use('ThePrimeagen/harpoon')
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
