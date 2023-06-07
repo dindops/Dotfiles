f165b75c9e6dad2d5256dbf92c347919ee1029a7
Piotr Dindorf <piotr.dindorf@mailo.com>
2023-06-07 22:45:06 +0200

    Fish improvements - fzf over hidden directories, cargo bin, aliases changed to abbreviations

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -12,12 +12,8 @@ fish_default_key_bindings
 # fzf.fish configs
 fzf_configure_bindings --directory=\cf --variables=\e\cv
 set fzf_directory_opts --bind "ctrl-o:execute(vim {} &> /dev/tty)"
+set fzf_fd_opts --hidden --exclude=.git
 
 # Aliases:
-alias proj='cd $HOME/Documents/Projects'
-alias wiki='vim $HOME/vimwiki/index.md'
-alias gwiki='cd $HOME/vimwiki/'
 alias bud='libreoffice $HOME/Documents/Personal/budget.ods'
-alias vim='nvim'
-alias vi='vim'
 alias f='nvim (fzf --preview "bat --color=always {}")'
