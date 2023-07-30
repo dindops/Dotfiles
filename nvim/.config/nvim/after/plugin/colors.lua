903e3336e4c37c56ab247a2e9e298dcf0165658f
Piotr Dindorf <piotr.dindorf@mailo.com>
2023-07-31 00:20:14 +0200

    Changes from dracula to solarized in kitty and neovim

--- nvim/.config/nvim/after/plugin/colors.lua
+++ nvim/.config/nvim/after/plugin/colors.lua
@@ -1,11 +1,11 @@
 function ColorschemeSetup(color)
-	color = color or "dracula"
+	color = color or "neosolarized"
 	vim.cmd.colorscheme(color)
 	vim.o.termguicolors = true
-  -- require('neosolarized').setup({
-  --   comment_italics = true,
-  --   background_set = true
-  -- })
+  require('neosolarized').setup({
+    comment_italics = true,
+    background_set = false
+  })
 
 end
 
