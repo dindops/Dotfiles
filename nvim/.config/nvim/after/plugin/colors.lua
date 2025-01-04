bf26f6f0e27c3fb8e9935be286d0f2bd35c9bedc
Piotr Dindorf <piotr.dindorf@mailo.com>
2025-01-04 22:55:25 +0100

    Small cleanup of the nvim colorscheme configs

--- nvim/.config/nvim/after/plugin/colors.lua
+++ nvim/.config/nvim/after/plugin/colors.lua
@@ -2,11 +2,6 @@ function ColorschemeSetup(color)
   color = color or "neosolarized"
   vim.cmd.colorscheme(color)
   vim.o.termguicolors = true
-  require('neosolarized').setup({
-    comment_italics = true,
-    background_set = false
-  })
-
 end
 
 ColorschemeSetup()
--- nvim/.config/nvim/lua/config/plugins/colorschemes.lua
+++ nvim/.config/nvim/lua/config/plugins/colorschemes.lua
@@ -2,6 +2,13 @@ return {
   {'ellisonleao/gruvbox.nvim'},
   {'Mofiqul/dracula.nvim'},
   {'svrana/neosolarized.nvim',
+  config = function()
+    require("neosolarized").setup({
+      comment_italics = true,
+      background_set = false,
+    })
+    vim.cmd.colorscheme("neosolarized")
+  end,
   dependencies = {
     {'tjdevries/colorbuddy.nvim'},
   }
