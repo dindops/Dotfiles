32a539028aac0684889cbf03853fd715f0aa8da5
Piotr Dindorf <piotr.dindorf@mailo.com>
2025-01-04 23:46:17 +0100

    Simplifies taskwarrior integration script

--- nvim/.config/nvim/lua/config/taskwarrior.lua
+++ nvim/.config/nvim/lua/config/taskwarrior.lua
@@ -12,11 +12,11 @@ function M.create_taskwarrior_task()
   local file_path = vim.fn.expand "%:p"
   local line_text = vim.fn.getline "."
   local keywords = { "TODO", "FIXME", "NOTE" }
-  print(line_text)
   line_text = trim(line_text)
   local task_description
   local task_tag
   for _, keyword in ipairs(keywords) do
+    print(line_text)
     local start_index, end_index = string.find(line_text, keyword)
     if start_index then
       task_description = string.sub(line_text, end_index + 2)
@@ -25,61 +25,41 @@ function M.create_taskwarrior_task()
     end
   end
   task_description = task_description or line_text
-  task_tag = task_tag or "inbox"
-  print(task_tag, task_description)
+  task_tag = task_tag or "+inbox"
 
-    -- Ask for project and other tags
   local project = vim.fn.input "Enter project name: "
   local additional_tags_input = vim.fn.input "Enter additional tags separated by spaces: "
   local additional_tags = {}
 
-  -- Prefix each additional tag with a "+"
   for tag in additional_tags_input:gmatch "%S+" do
     table.insert(additional_tags, "+" .. tag)
   end
 
   -- Construct the Taskwarrior command
-  -- Example: task add "This is the TODO text" +code nvimline:145:/home/user/file.lua
   local task_cmd = string.format(
     'task add %s "%s"',
     task_tag,
     task_description
   )
 
-  -- Add additional tags if available
   if #additional_tags > 0 then
     task_cmd = task_cmd .. " " .. table.concat(additional_tags, " ")
   end
 
-  -- Add project if available
+  local nvimline_annotation = string.format("nvimline:%d:%s", line_nr, file_path)
+  task_cmd = task_cmd .. " " .. nvimline_annotation
+
   if project and #project > 0 then
     task_cmd = task_cmd .. " project:" .. project
   end
 
-  -- Create task and then annotate it (currently annotations can be added after task creation)
   local output = vim.fn.system(task_cmd)
   print("Output: ", output)
-
-  for line in output:gmatch "[^\r\n]+" do
-    local task_id = string.match(line, "Created task (%d+)%.")
-    if task_id then
-      print("Task ID extracted: ", task_id)
-
-      -- Annotate task with filename and line number in the nvimline format
-      local nvimline_annotation = string.format("nvimline:%d:%s", line_nr, file_path)
-      local annotate_cmd = string.format('task %s annotate "%s"', task_id, nvimline_annotation)
-      local annotate_output = vim.fn.system(annotate_cmd)
-      print("Annotation output: ", annotate_output)
-      return
-    else
-      print "Failed to extract task ID"
-    end
-  end
 end
 
 vim.api.nvim_set_keymap(
   'n',
-  '<C-t>',
+  '<leader>ta',
   [[<cmd>lua require('config.taskwarrior').create_taskwarrior_task()<CR>]],
   { noremap = true, silent = true }
 )
