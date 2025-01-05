8a2591887117d6fd53270ff2483791cdfb79565d
Piotr Dindorf <piotr.dindorf@mailo.com>
2025-01-05 15:28:05 +0100

    Adds tmux-task-jump script and adds a small refactor to tmux-sessionizer

---
+++ scripts/.local/bin/_tmux_helper_functions.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+switch_to() {
+    if [[ -z $TMUX ]]; then
+        tmux attach-session -t $1
+    else
+        tmux switch-client -t $1
+    fi
+}
+
+has_session() {
+    tmux list-sessions | grep -q "^$1:"
+}
+
+hydrate() {
+    if [ -f $2/.tmux-sessionizer ]; then
+        tmux send-keys -t $1 "source $2/.tmux-sessionizer" c-M
+    elif [ -f $HOME/.tmux-sessionizer ]; then
+        tmux send-keys -t $1 "source $HOME/.tmux-sessionizer" c-M
+    fi
+}
+
---
+++ scripts/.local/bin/tmux-task-jump.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+source _tmux_helper_functions.sh
+
+# Usage: task jump <task_id>
+# 1. Extract the line & file path from task's description
+# 2. Launch Tmux, open nvim at that line in that file
+
+task_id="$1"
+if [ -z "$task_id" ]; then
+  echo "Usage: task jump <task_id>"
+  exit 1
+fi
+
+desc=$(task _get "$task_id".description)
+project=$(task _get "$task_id".project)
+
+nvimline=$(echo "$desc" | grep -o 'nvimline:[^ ]*')
+line=$(echo "$nvimline" | cut -d':' -f 2)
+filepath=$(echo "$nvimline" | cut -d':' -f 3-)
+
+if ! has_session $project; then
+    tmux new-session -ds $project "nvim +${line} ${filepath}"
+fi
+
+switch_to $project
