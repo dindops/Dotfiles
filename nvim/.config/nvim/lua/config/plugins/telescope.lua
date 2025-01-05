b6c6b6b823a0d980907bba0faec3b6cbbbfbc62a
Piotr Dindorf <piotr.dindorf@mailo.com>
2025-01-05 21:35:12 +0100

    Adjusts telescope to include hidden files + updates .gitignore

--- nvim/.config/nvim/lua/config/plugins/telescope.lua
+++ nvim/.config/nvim/lua/config/plugins/telescope.lua
@@ -8,7 +8,10 @@ return {
     },
     config = function()
       local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>ff', function()
+        builtin.find_files({hidden=true})
+      end, {}
+      )
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
       vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
       vim.keymap.set('n', '<leader>gs', function()
