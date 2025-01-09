8644828510fd05182fe730adf3d1dd13f0d074de
Piotr Dindorf <piotr.dindorf@mailo.com>
2025-01-09 23:02:12 +0100

    Changes default mapping for opening Oil.nvim to primageanian leader-pv

--- nvim/.config/nvim/after/plugin/oil.lua
+++ nvim/.config/nvim/after/plugin/oil.lua
@@ -3,4 +3,4 @@ require("oil").setup({
     show_hidden = true
   }
 })
-vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+vim.keymap.set("n", "<leader>pv", "<CMD>Oil<CR>", { desc = "Open parent directory" })
